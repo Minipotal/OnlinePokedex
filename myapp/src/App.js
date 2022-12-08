7bbbc8ac0962ecdaf2d2af23549b669d4264c901
Minipotal <Msylvestre52600@gmail.com>
2022-12-08 11:22:28 +0100

    maj
    Co-authored-by: Riuck01 <Riuck01@users.noreply.github.com>

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Pokedex from "./pages/Pokedex";
 import Caught from "./pages/Caught";
+import Nav from 'react-bootstrap/Nav';
 
 import {
   BrowserRouter as Router,
@@ -13,12 +14,12 @@ import {
 } from "react-router-dom";
 
 //App.js
-function App(props){
+function App(props) {
   return <Router>
-      <Switch>
-        <Route exact path="/"> {<Route path="/:id" children={<Home />} />/*ici on met l'URL dans le navigateur*/}
-          <Home /> {/*ici on donne la page à afficher en fonction de cette URL*/}
-        </Route>
+    <Switch>
+      <Route exact path="/"> {<Route path="/:id" children={<Home />} />/*ici on met l'URL dans le navigateur*/}
+        <Home /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+      </Route>
         <Route path="/about"> {<Route path="pages/:id" children={<About />} />}
           <About />
         </Route>
@@ -28,7 +29,7 @@ function App(props){
         <Route path="/caught"> {<Route path="pages/:id" children={<Caught />} />}
           <Caught />
         </Route>
-      </Switch>
+    </Switch>
   </Router>
 }
 
